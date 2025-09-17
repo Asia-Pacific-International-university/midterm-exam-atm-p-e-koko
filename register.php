12d0523a4e6b4af1a767b5edb257e0299af91746
p-e-koko <panneikoko1221@gmail.com>
2025-09-17 14:24:36 +0700

    Added registration form (POST handling)

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Registration Form</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="register.php" method="POST">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
