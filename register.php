b422675b6999da234b393ce59025b9a109ad954b
p-e-koko <panneikoko1221@gmail.com>
2025-09-21 22:09:40 +0700

    User registration saved into DB

--- register.php
+++ register.php
@@ -1,3 +1,30 @@
+<?php
+include "includes/db.php";
+
+$message = ""; // store success message
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name  = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO users (name, email, pin, balance) 
+            VALUES (:name, :email, :pin, :balance)";
+    $stmt = $pdo->prepare($sql);
+
+    if ($stmt->execute([
+        ':name'    => $name,
+        ':email'   => $email,
+        ':pin'     => password_hash("1234", PASSWORD_DEFAULT),
+        ':balance' => 0.00
+    ])) {
+        $message = "✅ Registration saved!";
+    } else {
+        $message = "❌ Registration failed!";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,13 +34,17 @@
     <title>Registration Form</title>
 </head>
 <body>
-    <h2>Register</h2>
     <form action="register.php" method="POST">
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name" required><br><br>
+        <h2>Register</h2>
+        <?php if ($message != ""): ?>
+            <p style="color: green; text-align:center;"><?php echo $message; ?></p>
+        <?php endif; ?>
+        
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
 
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
 
         <button type="submit">Register</button>
     </form>
